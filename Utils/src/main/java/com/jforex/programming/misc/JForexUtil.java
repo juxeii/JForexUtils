aae71beddf08d92237bdea8ef00c4460eb1fa6b6
juxeii <juxeii@juxe-PC.fritz.box>
2016-03-04 18:32:30 +0100

    Added accessor for IAccount instance.

--- Utils/src/main/java/com/jforex/programming/misc/JForexUtil.java
+++ Utils/src/main/java/com/jforex/programming/misc/JForexUtil.java
@@ -5,14 +5,6 @@ import java.util.concurrent.Executors;
 
 import org.aeonbits.owner.ConfigFactory;
 
-import com.dukascopy.api.IAccount;
-import com.dukascopy.api.IBar;
-import com.dukascopy.api.IContext;
-import com.dukascopy.api.IHistory;
-import com.dukascopy.api.IMessage;
-import com.dukascopy.api.ITick;
-import com.dukascopy.api.Instrument;
-import com.dukascopy.api.Period;
 import com.jforex.programming.instrument.InstrumentUtil;
 import com.jforex.programming.mm.RiskPercentMM;
 import com.jforex.programming.order.OrderChange;
@@ -31,6 +23,15 @@ import com.jforex.programming.quote.TickQuote;
 import com.jforex.programming.quote.TickQuoteProvider;
 import com.jforex.programming.settings.PlatformSettings;
 import com.jforex.programming.settings.UserSettings;
+
+import com.dukascopy.api.IAccount;
+import com.dukascopy.api.IBar;
+import com.dukascopy.api.IContext;
+import com.dukascopy.api.IHistory;
+import com.dukascopy.api.IMessage;
+import com.dukascopy.api.ITick;
+import com.dukascopy.api.Instrument;
+import com.dukascopy.api.Period;
 
 import rx.Observable;
 import rx.Subscription;
@@ -121,6 +122,10 @@ public class JForexUtil implements MessageConsumer {
 
     public IContext context() {
         return context;
+    }
+
+    public IAccount account() {
+        return account;
     }
 
     public Position position(final Instrument instrument) {
