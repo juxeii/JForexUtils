ec7451cb915012867874c4b297dc057fa73d8f71
juxeii <juxeii@juxe-PC.fritz.box>
2016-04-26 23:20:22 +0200

    Removal of order call result.

--- src/main/java/com/jforex/programming/order/OrderCreateResult.java
+++ src/main/java/com/jforex/programming/order/OrderCreateResult.java
@@ -2,21 +2,24 @@ package com.jforex.programming.order;
 
 import java.util.Optional;
 
-import com.jforex.programming.order.call.OrderCallResult;
-
 import com.dukascopy.api.IOrder;
 
-public final class OrderCreateResult extends OrderCallResult {
+public final class OrderCreateResult {
 
     private final Optional<IOrder> orderOpt;
+    private final Optional<Exception> exceptionOpt;
 
     public OrderCreateResult(final Optional<IOrder> orderOpt,
                              final Optional<Exception> exceptionOpt) {
-        super(exceptionOpt);
         this.orderOpt = orderOpt;
+        this.exceptionOpt = exceptionOpt;
     }
 
     public final Optional<IOrder> orderOpt() {
         return orderOpt;
+    }
+
+    public final Optional<Exception> exceptionOpt() {
+        return exceptionOpt;
     }
 }
