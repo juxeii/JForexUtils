e391899031e6d51066ae6f24e4154089e9c35607
juxeii <juxeii@juxe-PC.fritz.box>
2016-05-05 23:33:43 +0200

    Minor formatting.

--- src/main/java/com/jforex/programming/misc/JFEventPublisherForRx.java
+++ src/main/java/com/jforex/programming/misc/JFEventPublisherForRx.java
@@ -10,17 +10,8 @@ import rx.subscriptions.Subscriptions;
 
 public final class JFEventPublisherForRx<EventType> {
 
-    private final Observable<EventType> observable;
+    private final Observable<EventType> observable = Observable.create(subscriber -> subscribe(subscriber));
     private final Set<Subscriber<? super EventType>> subscribers = Sets.newConcurrentHashSet();
-
-    public JFEventPublisherForRx() {
-        observable = Observable.create(subscriber -> subscribe(subscriber));
-    }
-
-    private final void subscribe(final Subscriber<? super EventType> subscriber) {
-        subscriber.add(Subscriptions.create(() -> subscribers.remove(subscriber)));
-        subscribers.add(subscriber);
-    }
 
     public final Observable<EventType> observable() {
         return observable;
@@ -28,5 +19,10 @@ public final class JFEventPublisherForRx<EventType> {
 
     public final void onJFEvent(final EventType jfEvent) {
         subscribers.forEach(consumer -> consumer.onNext(jfEvent));
+    }
+
+    private final void subscribe(final Subscriber<? super EventType> subscriber) {
+        subscriber.add(Subscriptions.create(() -> subscribers.remove(subscriber)));
+        subscribers.add(subscriber);
     }
 }
