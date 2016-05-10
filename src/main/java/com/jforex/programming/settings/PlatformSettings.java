98116630d3e63c1e985aab403598a96adbc0c06a
reiss <reiss@5CG61348PG.nsn-intra.net>
2016-05-10 10:02:29 +0200

    Created properties files for settings.

--- src/main/java/com/jforex/programming/settings/PlatformSettings.java
+++ src/main/java/com/jforex/programming/settings/PlatformSettings.java
@@ -1,54 +1,72 @@
 package com.jforex.programming.settings;
 
 import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.Sources;
 
+@Sources({ "file:PlatformSettings.properties" })
 public interface PlatformSettings extends Config {
 
+    @Key("order.minslpipdistance")
     @DefaultValue("10.0")
     public double minSLPipDistance();
 
+    @Key("order.mintppipdistance")
     @DefaultValue("10.0")
     public double minTPPipDistance();
 
+    @Key("order.noslprice")
     @DefaultValue("0.0")
     public double noSLPrice();
 
+    @Key("order.notpprice")
     @DefaultValue("0.0")
     public double noTPPrice();
 
+    @Key("order.minamount")
     @DefaultValue("0.001")
     public double minAmount();
 
+    @Key("order.maxamount")
     @DefaultValue("1.0")
     public double maxAmount();
 
+    @Key("env.baseamount")
     @DefaultValue("1000000.0")
     public double baseAmount();
 
+    @Key("math.amountprecision")
     @DefaultValue("6")
     public int amountPrecision();
 
+    @Key("math.pipvalueprecision")
     @DefaultValue("4")
     public int pipValuePrecision();
 
+    @Key("math.pipfraction")
     @DefaultValue("1")
     public int pipFraction();
 
+    @Key("math.pipprecision")
     @DefaultValue("1")
     public int pipPrecision();
 
+    @Key("env.strategythreadprefix")
     @DefaultValue("Strategy")
     public String strategyThreadPrefix();
 
+    @Key("order.labelregex")
     @DefaultValue("\\w{1,256}")
     public String labelRegex();
 
+    @Key("order.maxretriesonfail")
     @DefaultValue("3")
     public int maxRetriesOnOrderFail();
 
+    @Key("order.retrydelayonfail")
     @DefaultValue("1500")
     public long delayOnOrderFailRetry();
 
+    @Key("env.terminationtimeoutexecutor")
     @DefaultValue("100")
     public long terminationTimeoutExecutorService();
 }
--- src/main/java/com/jforex/programming/settings/UserSettings.java
+++ src/main/java/com/jforex/programming/settings/UserSettings.java
@@ -1,24 +1,32 @@
 package com.jforex.programming.settings;
 
 import org.aeonbits.owner.Config;
+import org.aeonbits.owner.Config.Sources;
 
+@Sources({ "file:UserSettings.properties" })
 public interface UserSettings extends Config {
 
+    @Key("order.defaultsplippage")
     @DefaultValue("2.0")
     public double defaultSlippage();
 
+    @Key("order.defaultopenprice")
     @DefaultValue("0.0")
     public double defaultOpenPrice();
 
+    @Key("order.defaultgtt")
     @DefaultValue("0")
     public long defaultGTT();
 
+    @Key("order.defaultcomment")
     @DefaultValue("")
     public String defaultOrderComment();
 
+    @Key("order.defaultmergeprefix")
     @DefaultValue("M_")
     public String defaultMergePrefix();
 
+    @Key("env.enableweekendquotefilter")
     @DefaultValue("true")
     public boolean enableWeekendQuoteFilter();
 }
