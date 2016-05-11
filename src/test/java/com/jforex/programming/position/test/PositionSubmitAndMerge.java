6426770591a2457529a2a8a2cf4c03e5702620d1
reiss <reiss@5CG61348PG.nsn-intra.net>
2016-05-11 13:46:55 +0200

    Bug fix: former impl of submitAndMerge was not thread safe.

--- src/test/java/com/jforex/programming/position/test/PositionSubmitAndMerge.java
+++ src/test/java/com/jforex/programming/position/test/PositionSubmitAndMerge.java
@@ -4,26 +4,29 @@ import com.jforex.programming.order.OrderParams;
 import com.jforex.programming.position.Position;
 import com.jforex.programming.position.PositionEvent;
 
+import rx.Observable;
+
 public final class PositionSubmitAndMerge {
 
     private final Position position;
-    private String mergeLabel;
+    private final Observable<PositionEvent> submitDoneObs;
 
     public PositionSubmitAndMerge(final Position position) {
         this.position = position;
 
-        subscribeToPositionEvents();
-    }
-
-    private final void subscribeToPositionEvents() {
-        position.positionEventObs()
-                .filter(positonEvent -> positonEvent == PositionEvent.SUBMITTASK_DONE)
-                .subscribe(positonEvent -> position.merge(mergeLabel));
+        submitDoneObs = Observable.create(subscriber -> {
+            position.positionEventObs()
+                    .filter(positonEvent -> positonEvent == PositionEvent.SUBMITTASK_DONE)
+                    .subscribe(positonEvent -> {
+                        subscriber.onNext(positonEvent);
+                        subscriber.onCompleted();
+                    });
+        });
     }
 
     public final void submitAndMerge(final OrderParams orderParams,
                                      final String mergeLabel) {
-        this.mergeLabel = mergeLabel;
         position.submit(orderParams);
+        submitDoneObs.subscribe(positonEvent -> position.merge(mergeLabel));
     }
 }
