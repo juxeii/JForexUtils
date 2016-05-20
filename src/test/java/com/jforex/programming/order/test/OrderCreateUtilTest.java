606778c9bddad2449d3830ec312dcf8068dac730
reiss <reiss@5CG61348PG.nsn-intra.net>
2016-05-20 08:54:37 +0200

    Adapted UTs.

--- src/test/java/com/jforex/programming/order/test/OrderCreateUtilTest.java
+++ src/test/java/com/jforex/programming/order/test/OrderCreateUtilTest.java
@@ -1,11 +1,12 @@
 package com.jforex.programming.order.test;
 
-import static org.mockito.Matchers.any;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Optional;
-import java.util.function.Supplier;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,21 +15,17 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 
+import com.dukascopy.api.IOrder;
+import com.dukascopy.api.JFException;
+import com.google.common.collect.Sets;
 import com.jforex.programming.order.OrderCreateUtil;
 import com.jforex.programming.order.OrderParams;
 import com.jforex.programming.order.OrderUtilHandler;
-import com.jforex.programming.order.call.OrderCallExecutorResult;
-import com.jforex.programming.order.call.OrderCallRejectException;
-import com.jforex.programming.order.call.OrderCallRequest;
 import com.jforex.programming.order.call.OrderSupplierCall;
 import com.jforex.programming.order.event.OrderEvent;
-import com.jforex.programming.order.event.OrderEventType;
+import com.jforex.programming.order.event.OrderEventTypeData;
 import com.jforex.programming.test.common.InstrumentUtilForTest;
 import com.jforex.programming.test.common.OrderParamsForTest;
-import com.jforex.programming.test.fakes.IOrderForTest;
-
-import com.dukascopy.api.IOrder;
-import com.dukascopy.api.JFException;
 
 import de.bechte.junit.runners.context.HierarchicalContextRunner;
 import rx.Observable;
@@ -45,191 +42,101 @@ public class OrderCreateUtilTest extends InstrumentUtilForTest {
     private OrderUtilHandler orderUtilHandlerMock;
     @Captor
     private ArgumentCaptor<OrderSupplierCall> orderCallCaptor;
-    private Subject<OrderEvent, OrderEvent> orderEventSubject = PublishSubject.create();
-    private final IOrderForTest orderUnderTest = IOrderForTest.buyOrderEURUSD();
-    private OrderCallExecutorResult orderExecutorResult;
-    private OrderCallExecutorResult orderExecutorResultWithJFException;
+    @Captor
+    private ArgumentCaptor<OrderEventTypeData> typeDataCaptor;
+    private final Subject<OrderEvent, OrderEvent> orderEventSubject = PublishSubject.create();
 
     @Before
     public void setUp() {
         initCommonTestFramework();
 
-        orderEventSubject = PublishSubject.create();
-        orderExecutorResult = new OrderCallExecutorResult(Optional.of(orderUnderTest),
-                                                          Optional.empty());
-        orderExecutorResultWithJFException = new OrderCallExecutorResult(Optional.empty(),
-                                                                         Optional.of(jfException));
-        setUpMocks();
-
         orderCreateUtil = new OrderCreateUtil(engineMock, orderUtilHandlerMock);
     }
 
-    private void setUpMocks() {
-        when(orderEventGatewayMock.observable()).thenReturn(orderEventSubject);
-        when(orderCallExecutorMock.run(any(OrderSupplierCall.class))).thenReturn(orderExecutorResult);
-    }
-
-    private void prepareJFException() {
-        when(orderCallExecutorMock.run(any(OrderSupplierCall.class)))
-                .thenReturn(orderExecutorResultWithJFException);
-    }
-
     private void captureAndRunOrderCall() throws JFException {
-        verify(orderCallExecutorMock).run(orderCallCaptor.capture());
+        verify(orderUtilHandlerMock).runOrderSupplierCall(orderCallCaptor.capture(), typeDataCaptor.capture());
         orderCallCaptor.getValue().get();
     }
 
-    private void assertJFException(final TestSubscriber<?> subscriber) {
-        subscriber.assertValueCount(0);
-        subscriber.assertError(JFException.class);
-    }
+    public class SubmitSetup {
 
-    private void assertOtherOrderIsIgnored(final TestSubscriber<OrderEvent> subscriber) {
-        orderEventSubject.onNext(new OrderEvent(IOrderForTest.orderAUDUSD(), OrderEventType.MERGE_OK));
-        subscriber.assertNoErrors();
-        subscriber.assertNotCompleted();
-    }
-
-    private void sendOrderEvent(final IOrder order,
-                                final OrderEventType orderEventType) {
-        orderEventSubject.onNext(new OrderEvent(order, orderEventType));
-    }
-
-    private void assertSubscriberCompleted(final TestSubscriber<?> subscriber) {
-        subscriber.assertNoErrors();
-        subscriber.assertCompleted();
-    }
-
-    public class BuySubmitSetup {
-
-        private final IOrderForTest buyOrder = IOrderForTest.buyOrderEURUSD();
         private final OrderParams orderParamsBUY = OrderParamsForTest.paramsBuyEURUSD();
-        private final TestSubscriber<OrderEvent> buySubmitSubscriber = new TestSubscriber<>();
-        private Observable<OrderEvent> buyObservable;
-        private final Supplier<Observable<OrderEvent>> buySubmitCall =
-                () -> buyObservable = orderCreateUtil.submitOrder(orderParamsBUY);
-
-        public class JFExceptionOnSubmit {
-
-            @Before
-            public void setUp() {
-                prepareJFException();
-
-                buySubmitCall.get();
-            }
-
-            @Test
-            public void testSubmitCallsOnIEngine() throws JFException {
-                captureAndRunOrderCall();
-
-                engineForTest.verifySubmit(orderParamsBUY, 1);
-            }
-
-            @Test
-            public void testBuySubscriberCompletedWithError() {
-                buyObservable.subscribe(buySubmitSubscriber);
-
-                assertJFException(buySubmitSubscriber);
-            }
-        }
+        private final TestSubscriber<OrderEvent> submitSubscriber = new TestSubscriber<>();
+        private final Observable<OrderEvent> submitObservable = Observable.empty();
+        private final OrderEventTypeData submitTypeData = OrderEventTypeData.submitData;
 
         public class SubmitCall {
 
             @Before
-            public void setUp() {
-                final OrderCallExecutorResult orderExecutorResult =
-                        new OrderCallExecutorResult(Optional.of(buyOrder), Optional.empty());
-                when(orderCallExecutorMock.run(any(OrderSupplierCall.class)))
-                        .thenReturn(orderExecutorResult);
+            public void setUp() throws JFException {
+                when(orderUtilHandlerMock
+                        .runOrderSupplierCall(orderCallCaptor.capture(), eq(submitTypeData)))
+                                .thenReturn(submitObservable);
 
-                buySubmitCall.get();
+                orderCreateUtil.submitOrder(orderParamsBUY).subscribe(submitSubscriber);
 
-                buyOrder.setState(IOrder.State.CREATED);
+                captureAndRunOrderCall();
             }
 
             @Test
             public void testSubmitCallsOnIEngine() throws JFException {
-                captureAndRunOrderCall();
-
                 engineForTest.verifySubmit(orderParamsBUY, 1);
             }
 
             @Test
-            public void testBuyOrderIsRegisteredOnEventGateway() {
-                verify(orderEventGatewayMock).registerOrderRequest(buyOrder, OrderCallRequest.SUBMIT);
+            public void testSubmitCallsHandlerWithCorrectTypeData() {
+                final OrderEventTypeData orderEventTypeData = typeDataCaptor.getValue();
+
+                assertThat(orderEventTypeData, equalTo(submitTypeData));
             }
 
             @Test
-            public void testSubmitObservableIgnoresOtherOrders() {
-                assertOtherOrderIsIgnored(buySubmitSubscriber);
+            public void testSuscriberIsNotifiesOnEvent() {
+                orderEventSubject.onCompleted();
+
+                submitSubscriber.assertCompleted();
             }
+        }
+    }
 
-            public class WhenSubmitRejected {
+    public class MergeSetup {
 
-                @Before
-                public void setUp() {
-                    buyOrder.setState(IOrder.State.CANCELED);
+        private final Set<IOrder> mergeOrders = Sets.newHashSet();
+        private final String mergeOrderLabel = "MergeLabel";
+        private final TestSubscriber<OrderEvent> mergeSubscriber = new TestSubscriber<>();
+        private final Observable<OrderEvent> mergeObservable = Observable.empty();
+        private final OrderEventTypeData mergeTypeData = OrderEventTypeData.mergeData;
 
-                    sendOrderEvent(buyOrder, OrderEventType.SUBMIT_REJECTED);
-                }
+        public class MergeCall {
 
-                @Test
-                public void testBuySubscriberCompletedWithRejection() {
-                    buyObservable.subscribe(buySubmitSubscriber);
+            @Before
+            public void setUp() throws JFException {
+                when(orderUtilHandlerMock
+                        .runOrderSupplierCall(orderCallCaptor.capture(), eq(mergeTypeData)))
+                                .thenReturn(mergeObservable);
 
-                    buySubmitSubscriber.assertError(OrderCallRejectException.class);
-                }
+                orderCreateUtil.mergeOrders(mergeOrderLabel, mergeOrders).subscribe(mergeSubscriber);
 
-                @Test
-                public void testNoRetryIsDone() throws JFException {
-                    captureAndRunOrderCall();
-
-                    engineForTest.verifySubmit(orderParamsBUY, 1);
-                }
-            }
-
-            public class WhenFillRejected {
-
-                @Before
-                public void setUp() {
-                    buyOrder.setState(IOrder.State.CANCELED);
-
-                    sendOrderEvent(buyOrder, OrderEventType.FILL_REJECTED);
-                }
-
-                @Test
-                public void testBuySubscriberCompletedWithRejection() {
-                    buyObservable.subscribe(buySubmitSubscriber);
-
-                    buySubmitSubscriber.assertError(OrderCallRejectException.class);
-                }
-
-                @Test
-                public void testNoRetryIsDone() throws JFException {
-                    captureAndRunOrderCall();
-
-                    engineForTest.verifySubmit(orderParamsBUY, 1);
-                }
+                captureAndRunOrderCall();
             }
 
             @Test
-            public void testOnSubmitDone() {
-                buyObservable.subscribe(buySubmitSubscriber);
-
-                sendOrderEvent(buyOrder, OrderEventType.SUBMIT_OK);
-
-                buySubmitSubscriber.assertNoErrors();
-                buySubmitSubscriber.assertNotCompleted();
+            public void testMergeCallsOnIEngine() throws JFException {
+                engineForTest.verifyMerge(mergeOrderLabel, mergeOrders, 1);
             }
 
             @Test
-            public void testOnFilled() {
-                buyOrder.setState(IOrder.State.FILLED);
-                buyObservable.subscribe(buySubmitSubscriber);
+            public void testMergeCallsHandlerWithCorrectTypeData() {
+                final OrderEventTypeData orderEventTypeData = typeDataCaptor.getValue();
 
-                sendOrderEvent(buyOrder, OrderEventType.FULL_FILL_OK);
+                assertThat(orderEventTypeData, equalTo(mergeTypeData));
+            }
 
-                assertSubscriberCompleted(buySubmitSubscriber);
+            @Test
+            public void testSuscriberIsNotifiesOnEvent() {
+                orderEventSubject.onCompleted();
+
+                mergeSubscriber.assertCompleted();
             }
         }
     }
