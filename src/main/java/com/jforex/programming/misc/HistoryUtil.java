d499d6acc8ce37a2158a161b252f3fa21b92f9f9
reiss <reiss@5CG61348PG.nsn-intra.net>
2016-06-17 10:52:44 +0200

    Simplifications.

--- src/main/java/com/jforex/programming/misc/HistoryUtil.java
+++ src/main/java/com/jforex/programming/misc/HistoryUtil.java
@@ -1,9 +1,10 @@
 package com.jforex.programming.misc;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,42 +31,50 @@ public class HistoryUtil {
     }
 
     public Map<Instrument, TickQuote> tickQuotes(final Set<Instrument> instruments) {
-        final Map<Instrument, TickQuote> quoteByInstrument = new ConcurrentHashMap<>();
-        instruments.forEach(instrument -> quoteByInstrument.put(instrument,
-                                                                new TickQuote(instrument, latestTick(instrument))));
-        return quoteByInstrument;
+        return instruments.stream()
+                .map(instrument -> {
+                    final TickQuote tickQuote = new TickQuote(instrument, latestTick(instrument));
+                    return new SimpleEntry<>(instrument, tickQuote);
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     public ITick latestTick(final Instrument instrument) {
         return Observable.fromCallable(() -> history.getLastTick(instrument))
-                .flatMap(tick -> {
-                    if (tick == null) {
-                        logger.warn("Last tick for " + instrument + " from history returned null! Retrying...");
-                        return Observable.error(new QuoteProviderException("History tick is null!"));
-                    }
-                    return Observable.just(tick);
-                })
+                .flatMap(tick -> tickObservableForHistoryTick(instrument, tick))
+                .doOnError(e -> logger.warn("Last tick for " + instrument + " from history returned null! Retrying..."))
                 .retryWhen(errors -> RxUtil.retryWithDelay(errors, 500L, TimeUnit.MILLISECONDS, 10))
                 .first()
                 .toBlocking()
                 .first();
     }
 
+    private Observable<ITick> tickObservableForHistoryTick(final Instrument instrument,
+                                                           final ITick tick) {
+        return tick == null
+                ? Observable.error(new QuoteProviderException("History tick is null!"))
+                : Observable.just(tick);
+    }
+
     public IBar latestBar(final Instrument instrument,
                           final Period period,
                           final OfferSide offerSide) {
         return Observable.fromCallable(() -> history.getBar(instrument, period, offerSide, 1))
-                .flatMap(bar -> {
-                    if (bar == null) {
-                        logger.error("Last bar for " + instrument + " and period " + period
-                                     + " and offerside " + offerSide + " from history returned null!");
-                        return Observable.error(new QuoteProviderException("History bar is null!"));
-                    }
-                    return Observable.just(bar);
-                })
+                .flatMap(bar -> barObservableForHistoryBar(instrument, period, offerSide, bar))
+                .doOnError(e -> logger.error("Last bar for " + instrument + " and period " + period
+                        + " and offerside " + offerSide + " from history returned null!"))
                 .retry(10)
                 .onErrorResumeNext(e -> Observable.error(new QuoteProviderException(e.getMessage())))
                 .toBlocking()
                 .first();
+    }
+
+    private Observable<IBar> barObservableForHistoryBar(final Instrument instrument,
+                                                        final Period period,
+                                                        final OfferSide offerSide,
+                                                        final IBar bar) {
+        return bar == null
+                ? Observable.error(new QuoteProviderException("History bar is null!"))
+                : Observable.just(bar);
     }
 }
