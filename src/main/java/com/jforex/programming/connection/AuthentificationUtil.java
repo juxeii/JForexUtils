2a88ec68249d94538b27111e69d306e82e0e5b5e
reiss <reiss@5CG61348PG.nsn-intra.net>
2016-06-28 13:42:43 +0200

    Simplifications.

--- src/main/java/com/jforex/programming/connection/AuthentificationUtil.java
+++ src/main/java/com/jforex/programming/connection/AuthentificationUtil.java
@@ -5,20 +5,21 @@ import java.util.concurrent.TimeUnit;
 
 import org.aeonbits.owner.ConfigFactory;
 
+import com.dukascopy.api.system.IClient;
 import com.github.oxo42.stateless4j.StateMachine;
 import com.github.oxo42.stateless4j.StateMachineConfig;
 import com.jforex.programming.misc.JFHotSubject;
 import com.jforex.programming.settings.PlatformSettings;
 
-import com.dukascopy.api.system.IClient;
-
 import rx.Completable;
+import rx.Completable.CompletableSubscriber;
 import rx.Observable;
+import rx.Subscription;
 
 public class AuthentificationUtil {
 
     private final IClient client;
-    private final JFHotSubject<LoginState> loginStatePublisher = new JFHotSubject<>();
+    private final JFHotSubject<LoginState> loginStateSubject = new JFHotSubject<>();
     private final Observable<ConnectionState> connectionStateObs;
     private final StateMachineConfig<LoginState, FSMTrigger> fsmConfig = new StateMachineConfig<>();
     private final StateMachine<LoginState, FSMTrigger> fsm = new StateMachine<>(LoginState.LOGGED_OUT, fsmConfig);
@@ -51,20 +52,20 @@ public class AuthentificationUtil {
 
     private final void configureFSM() {
         fsmConfig.configure(LoginState.LOGGED_OUT)
-                .onEntry(() -> loginStatePublisher.onNext(LoginState.LOGGED_OUT))
+                .onEntry(() -> loginStateSubject.onNext(LoginState.LOGGED_OUT))
                 .permit(FSMTrigger.CONNECTED, LoginState.LOGGED_IN)
                 .ignore(FSMTrigger.DISCONNECTED)
                 .ignore(FSMTrigger.LOGOUT);
 
         fsmConfig.configure(LoginState.LOGGED_IN)
-                .onEntry(() -> loginStatePublisher.onNext(LoginState.LOGGED_IN))
+                .onEntry(() -> loginStateSubject.onNext(LoginState.LOGGED_IN))
                 .permit(FSMTrigger.LOGOUT, LoginState.LOGGED_OUT)
                 .ignore(FSMTrigger.CONNECTED)
                 .ignore(FSMTrigger.DISCONNECTED);
     }
 
     public final Observable<LoginState> loginStateObs() {
-        return loginStatePublisher.observable();
+        return loginStateSubject.observable();
     }
 
     public LoginState loginState() {
@@ -72,34 +73,39 @@ public class AuthentificationUtil {
     }
 
     public Completable login(final LoginCredentials loginCredentials) {
-        final Optional<Exception> maybeException = connectClient(loginCredentials.jnlpAddress(),
-                                                                 loginCredentials.username(),
-                                                                 loginCredentials.password(),
-                                                                 loginCredentials.maybePin());
+        final Optional<Exception> maybeException = connectClient(loginCredentials);
         return completableFromConnectResult(maybeException);
     }
 
     private Completable completableFromConnectResult(final Optional<Exception> maybeException) {
         return maybeException.isPresent()
                 ? Completable.error(maybeException.get())
-                : Completable.create(subscriber -> {
-                    connectionStateObs.take(1)
-                            .subscribe(connectionState -> {
-                                if (connectionState == ConnectionState.CONNECTED)
-                                    subscriber.onCompleted();
-                                else
-                                    subscriber.onError(new ConnectException());
-                            });
-                }).timeout(platformSettings.logintimeoutseconds(), TimeUnit.SECONDS);
+                : Completable
+                        .create(this::loginSubscription)
+                        .timeout(platformSettings.logintimeoutseconds(), TimeUnit.SECONDS);
     }
 
-    private final Optional<Exception> connectClient(final String jnlpAddress,
-                                                    final String username,
-                                                    final String password,
-                                                    final Optional<String> maybePin) {
+    private final Subscription loginSubscription(final CompletableSubscriber subscriber) {
+        return connectionStateObs.take(1)
+                .subscribe(connectionState -> processConnectionStateForLogin(connectionState, subscriber));
+    }
+
+    private final void processConnectionStateForLogin(final ConnectionState connectionState,
+                                                      final CompletableSubscriber subscriber) {
+        if (connectionState == ConnectionState.CONNECTED)
+            subscriber.onCompleted();
+        else
+            subscriber.onError(new ConnectException());
+    }
+
+    private final Optional<Exception> connectClient(final LoginCredentials loginCredentials) {
+        final String jnlpAddress = loginCredentials.jnlpAddress();
+        final String username = loginCredentials.username();
+        final String password = loginCredentials.password();
+
         try {
-            if (maybePin.isPresent())
-                client.connect(jnlpAddress, username, password, maybePin.get());
+            if (loginCredentials.maybePin().isPresent())
+                client.connect(jnlpAddress, username, password, loginCredentials.maybePin().get());
             else
                 client.connect(jnlpAddress, username, password);
         } catch (final Exception e) {
