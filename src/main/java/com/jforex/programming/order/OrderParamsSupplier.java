73191c3ab2e9de92dcc21be72835ea322aef22f7
juxeii <juxeii@juxe-PC.fritz.box>
2016-08-04 23:01:36 +0200

    Removed paramsSupplier.

--- src/main/java/com/jforex/programming/order/OrderParamsSupplier.java
+++
@@ -1,9 +0,0 @@
-package com.jforex.programming.order;
-
-import com.dukascopy.api.IEngine.OrderCommand;
-
-@FunctionalInterface
-public interface OrderParamsSupplier {
-
-    public OrderParams forCommand(OrderCommand orderCommand);
-}
