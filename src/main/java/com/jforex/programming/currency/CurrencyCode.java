b9927d35ec7a46bbdd9733caee872e7b7e05f248
reiss <reiss@5CG61348PG.nsn-intra.net>
2016-10-19 09:21:41 +0200

    Added javadoc for currency codes.

--- src/main/java/com/jforex/programming/currency/CurrencyCode.java
+++ src/main/java/com/jforex/programming/currency/CurrencyCode.java
@@ -2,27 +2,50 @@ package com.jforex.programming.currency;
 
 public enum CurrencyCode {
 
-    EUR,
-    CHF,
-    USD,
-    GBP,
-    JPY,
+    /** Currency code for the Australian Dollar. */
     AUD,
-    NZD,
-    CAD,
-    HKD,
-    SGD,
-    SEK,
-    CZK,
-    RON,
-    NOK,
-    TRY,
-    RUB,
-    CNH,
-    DKK,
-    HUF,
-    PLN,
+    /** Currency code for the Brazilian Real. */
     BRL,
+    /** Currency code for the Canadian Dollar. */
+    CAD,
+    /** Currency code for the Swiss Franc. */
+    CHF,
+    /** Currency code for the Chinese Yuan. */
+    CNH,
+    /** Currency code for the Czech Koruna. */
+    CZK,
+    /** Currency code for the Danish Krone. */
+    DKK,
+    /** Currency code for the Euro. */
+    EUR,
+    /** Currency code for the British Pound. */
+    GBP,
+    /** Currency code for the Hongkong Dollar. */
+    HKD,
+    /** Currency code for the Hungarian Forint. */
+    HUF,
+    /** Currency code for the Japanese Yen. */
+    JPY,
+    /** Currency code for the Mexican Peso. */
     MXN,
+    /** Currency code for the Norwegian Krone. */
+    NOK,
+    /** Currency code for the New Zealand Dollar. */
+    NZD,
+    /** Currency code for the Polish Zloty. */
+    PLN,
+    /** Currency code for the Romanian Leu. */
+    RON,
+    /** Currency code for the Russian Ruble. */
+    RUB,
+    /** Currency code for the Swedish Krona. */
+    SEK,
+    /** Currency code for the Singapur Dollar. */
+    SGD,
+    /** Currency code for the Turkish Lira. */
+    TRY,
+    /** Currency code for the US Dollar. */
+    USD,
+    /** Currency code for the South African Rand. */
     ZAR
 }
