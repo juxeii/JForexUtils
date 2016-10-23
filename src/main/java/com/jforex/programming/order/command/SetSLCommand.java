03dcd8ba1d98816cfa75a0d869c5cee3ede02798
juxeii <juxeii@juxe-PC.fritz.box>
2016-10-23 20:11:31 +0200

    Added a SLCommand builder. Will be later used for OrderUtil; no tests yet.

---
+++ src/main/java/com/jforex/programming/order/command/SetSLCommand.java
@@ -0,0 +1,90 @@
+package com.jforex.programming.order.command;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.dukascopy.api.IOrder;
+import com.dukascopy.api.OfferSide;
+
+public class SetSLCommand {
+
+    private final IOrder order;
+    private final double newSL;
+    private final OfferSide offerSide;
+    private final double trailingStep;
+
+    public interface SLOption {
+
+        public SLOption withOfferSide(OfferSide offerSide);
+
+        public SLOption withTrailingStep(double trailingStep);
+
+        public SetSLCommand build();
+    }
+
+    private SetSLCommand(final Builder builder) {
+        order = builder.order;
+        newSL = builder.newSL;
+        offerSide = builder.offerSide;
+        trailingStep = builder.trailingStep;
+    }
+
+    public final IOrder order() {
+        return order;
+    }
+
+    public double newSL() {
+        return newSL;
+    }
+
+    public OfferSide offerSide() {
+        return offerSide;
+    }
+
+    public double trailingStep() {
+        return trailingStep;
+    }
+
+    public static SLOption newBuilder(final IOrder order,
+                                      final double newSL) {
+        checkNotNull(order);
+
+        return new Builder(order, newSL);
+    }
+
+    public static class Builder implements SLOption {
+
+        private final IOrder order;
+        private final double newSL;
+        private OfferSide offerSide;
+        private double trailingStep = -1;
+
+        public Builder(final IOrder order,
+                       final double newSL) {
+            this.order = order;
+            this.newSL = newSL;
+
+            offerSide = order.isLong()
+                    ? OfferSide.BID
+                    : OfferSide.ASK;
+        }
+
+        @Override
+        public SLOption withOfferSide(final OfferSide offerSide) {
+            checkNotNull(offerSide);
+
+            this.offerSide = offerSide;
+            return this;
+        }
+
+        @Override
+        public SLOption withTrailingStep(final double trailingStep) {
+            this.trailingStep = trailingStep;
+            return this;
+        }
+
+        @Override
+        public SetSLCommand build() {
+            return new SetSLCommand(this);
+        }
+    }
+}
