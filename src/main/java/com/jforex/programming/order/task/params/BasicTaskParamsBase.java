654880e409daaf6c1cf5694efc865f8b2bb6701f
reiss <reiss@5CG61348PG.nsn-intra.net>
2017-02-08 10:38:15 +0100

    BasicTaskParamsBase now extends from TaskComposeAndEventMapData

--- src/main/java/com/jforex/programming/order/task/params/BasicTaskParamsBase.java
+++ src/main/java/com/jforex/programming/order/task/params/BasicTaskParamsBase.java
@@ -1,29 +1,14 @@
 package com.jforex.programming.order.task.params;
 
-import java.util.Map;
-import java.util.function.Consumer;
-
-import com.jforex.programming.order.event.OrderEvent;
-import com.jforex.programming.order.event.OrderEventType;
 import com.jforex.programming.order.task.params.basic.BasicParamsBuilder;
 
-public abstract class BasicTaskParamsBase implements TaskParamsBase {
-
-    protected TaskComposeAndEventMapData taskComposeAndEventMapData;
+public abstract class BasicTaskParamsBase extends TaskComposeAndEventMapData
+                                          implements TaskParamsBase {
 
     protected BasicTaskParamsBase(final BasicParamsBuilder<?> builder) {
-        taskComposeAndEventMapData = new TaskComposeAndEventMapData(builder);
+        super(builder);
     }
 
     @Override
     public abstract TaskParamsType type();
-
-    @Override
-    public ComposeData composeData() {
-        return taskComposeAndEventMapData.composeData();
-    }
-
-    public Map<OrderEventType, Consumer<OrderEvent>> consumerForEvent() {
-        return taskComposeAndEventMapData.consumerForEvent();
-    }
 }
